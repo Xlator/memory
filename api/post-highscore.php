4890e8c190b5e804eb87e752869d883644d10dba
Viktor Jackson <viktor.jackson@gmail.com>
2012-09-06 22:34:46 +0200

    Fix null insertion bug in post-highscore
    Thanks for the bug report Harald!

--- api/post-highscore.php
+++ api/post-highscore.php
@@ -2,6 +2,8 @@
 require_once("db.php");
 
 $query = "INSERT INTO log (clicks, end) VALUES (:score, datetime('now'))";
-$parameters = array("score" => $_POST['score']);
-
-echo json_encode(Database::executeNonQuery($query, $parameters));
+if(!is_null($_POST['score'])) {
+    $parameters = array("score" => $_POST['score']);
+    echo json_encode(Database::executeNonQuery($query, $parameters));
+    return;
+}
